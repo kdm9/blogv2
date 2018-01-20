404683976a37d1476e5229eed874b6f83b5568ee
Kevin Murray <kdmfoss@gmail.com>
2018-01-20 13:50:12 -0800

    Remove SITEURL for testing on netlify

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.kdmurray.id.au'
+SITEURL = ''#https://www.kdmurray.id.au'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
